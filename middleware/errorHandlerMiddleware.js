1393bbefbfca31339c24f26fa8eb9dc545c3ab20
Bohdan <kosturik.bohdan@gmail.com>
2024-02-05 22:12:55 +0100

    wrongly placed dist folder

--- middleware/errorHandlerMiddleware.js
+++ middleware/errorHandlerMiddleware.js
@@ -1,5 +1,7 @@
 const errorHandlerMiddleware = (err, req, res, next) => {
-  res.status(500).json({ msg: "Something went wrong - error handler" });
+  console.log(err);
+
+  return res.status(500).json({ msg: "Something went wrong - error handler" });
 };
 
 module.exports = errorHandlerMiddleware;
